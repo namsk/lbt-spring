d049896e7af88a18cf6324a016985bf36b8fc089
Sean Nam <skyun.nam@gmail.com>
2021-04-04 17:35:35 +0900

    Update Guestbook Oracle SQL

--- sql/guestbook-oracle.sql
+++ sql/guestbook-oracle.sql
@@ -3,10 +3,14 @@ CREATE TABLE guestbook (
     name varchar(20) NOT NULL,
     password varchar(20) NOT NULL,
     content varchar(255) NOT NULL,
-    regdate datetime DEFAULT regdate
+    regdate date DEFAULT sysdate
 );
 
-insert into guestbook (name, password, content)
-values ('방문자', 'test', '테스트 방명록입니다.');
+CREATE SEQUENCE seq_guestbook_no 
+    START WITH 1 INCREMENT BY 1 NOCACHE;
+
+insert into guestbook (no, name, password, content)
+values (seq_guestbook_no.NEXTVAL, '방문자', 'test', '테스트 방명록입니다.');
 
 SELECT no, name, password, content FROM guestbook ORDER BY regdate DESC;
+commit;
